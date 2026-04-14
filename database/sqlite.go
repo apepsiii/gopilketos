2a1b7a262b3399ac4cd1f148968f8c813275b6b8
unknown <m.saepurah24@gmail.com>
2026-04-14 17:27:01 +0700

    Fix SQLite connection with WAL mode and proper DSN
    - Add _pragma parameters for WAL journal mode
    - Set busy_timeout and foreign_keys pragma
    - Set MaxOpenConns=1 for SQLite compatibility
    - Fix 'out of memory' error on VPS deployment

--- database/sqlite.go
+++ database/sqlite.go
@@ -3,9 +3,10 @@ package database
 import (
 	"database/sql"
 	"log"
-	_ "modernc.org/sqlite"
 	"os"
 	"path/filepath"
+
+	_ "modernc.org/sqlite"
 )
 
 func InitDB(dbPath string) *sql.DB {
@@ -14,10 +15,15 @@ func InitDB(dbPath string) *sql.DB {
 		log.Fatalf("failed to create database directory: %v", err)
 	}
 
-	db, err := sql.Open("sqlite", dbPath)
+	dsn := dbPath + "?_pragma=journal_mode(WAL)&_pragma=busy_timeout(5000)&_pragma=foreign_keys(1)"
+
+	db, err := sql.Open("sqlite", dsn)
 	if err != nil {
 		log.Fatalf("failed to open database: %v", err)
 	}
+
+	db.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(1)
 
 	if err := db.Ping(); err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
